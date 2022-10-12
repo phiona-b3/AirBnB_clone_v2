893071d695069ed335212d15698271f453693139
phiona-b3 <phiona030303@gmail.com>
2022-10-12 12:57:12 -0700

    Task 7

--- models/user.py
+++ models/user.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = column(string(128), nullable=False)
-        password = column(string(128), nullable=False)
-        first_name = column(string(128), nullable=False)
-        last_name = column(string(128), nullable=False)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = 'users'
+    email = column(string(128), nullable=False)
+    password = column(string(128), nullable=False)
+    first_name = column(string(128), nullable=True)
+    last_name = column(string(128), nullable=True)
