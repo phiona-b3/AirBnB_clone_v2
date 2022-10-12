9d7cb5f7e3e1db8cdadd202296fdd910d189477d
phiona-b3 <phiona030303@gmail.com>
2022-10-12 12:43:47 -0700

    Task 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if models.storage_t == db:
+        __tablename__ = 'users'
+        email = column(string(128), nullable=False)
+        password = column(string(128), nullable=False)
+        first_name = column(string(128), nullable=False)
+        last_name = column(string(128), nullable=False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
