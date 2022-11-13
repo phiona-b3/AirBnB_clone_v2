28eaac05f9e8b733d66a230302e4e6e4e8205b2f
phiona-b3 <phiona030303@gmail.com>
2022-11-13 13:15:28 -0800

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""starting a flask web application"""
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """return hello hbnb!"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
