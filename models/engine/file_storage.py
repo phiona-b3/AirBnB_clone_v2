9ddf23bf35e5d0fd00a6884a76af8b7bfa09a5ee
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-11-16 16:21:35 +0300

    Task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
                 self.save()
+                
+    def close(self):
+        """method for deserializing the JSON file to objects"""
+        self.reload()
